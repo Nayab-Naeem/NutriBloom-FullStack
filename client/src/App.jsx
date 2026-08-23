02d9e219deec65e54c26cc860ae59de2d4707720
Nayab Naeem <nayabnaeem666@gmail.com>
2026-08-23 23:48:29 +0500

    Initial commit - clean project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-green-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-700">
+        NutriBloom
+      </h1>
+    </div>
+  )
+}
+
+export default App
